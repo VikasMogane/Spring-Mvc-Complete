8fef5a42d45b26fad353ea38e8033759f31f2706
vikas <moganevikas@gmail.com>
2021-04-04 02:58:32 +0530

    it will return car as hard coded dependency

---
+++ IOC/src/Bike.java
@@ -0,0 +1,10 @@
+
+public class Bike implements Engine{
+
+	@Override
+	public void start() 
+	{
+		System.out.println("Bike has been started");	
+	}
+
+}
---
+++ IOC/src/Car.java
@@ -0,0 +1,10 @@
+
+public class Car  implements Engine{
+
+	@Override
+	public void start() 
+	{
+		System.out.println("car has been started");
+	}
+
+}
