faba7008bcb54c5561f2475978bfbae0916d3dd8
vikas <moganevikas@gmail.com>
2021-04-04 16:18:35 +0530

    setter removed and only added @value tag

--- InjectLiteral/src/com/inject/literal/Employee.java
+++ InjectLiteral/src/com/inject/literal/Employee.java
@@ -4,23 +4,16 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Employee {
 	
+	@Value("${employee.name}")
 	public String empName;
+	
+	@Value("${employee.id}")
 	public int empId;
+	
+	@Value("${employee.skills}")
 	public String empOtherSkills;
 	
-	@Value("${employee.name}")
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-	@Value("${employee.id}")
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
-	@Value("${employee.skills}")
-	public void setEmpOtherSkills(String empOtherSkills) {
-		this.empOtherSkills = empOtherSkills;
-	}
-	
+
 	public void showDetails()
 	{
 		System.out.println("Emaployee name is = "+empName);
