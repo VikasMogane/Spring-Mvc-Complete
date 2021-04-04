29427b20f94c8485114db35c10005f00e0705a20
vikas <moganevikas@gmail.com>
2021-04-04 17:58:16 +0530

    @qualifier annotation

--- AnnotationOnly/src/com/vikas/annotation/Company.java
+++ AnnotationOnly/src/com/vikas/annotation/Company.java
@@ -1,12 +1,14 @@
 package com.vikas.annotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component 
 public class Company {
 
 	@Autowired
+	@Qualifier("hardwareEnginer")
 	Engineer engineer;
 
 	public void showInfo()
