1e618cab2c8513d520f5dd4fd5ec9b7ad060b20b
vikas <moganevikas@gmail.com>
2021-04-04 15:53:21 +0530

    no need setter and constructor in this approach

--- AutoWiring/src/com/autowire/Company.java
+++ AutoWiring/src/com/autowire/Company.java
@@ -4,26 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Company {
-	HrDept dept;
-	
-	public Company() {}
-	
-	
-	public Company(HrDept dept)
-	{
-		this.dept = dept;
-		System.out.println(" company constructor !! ");
 
-		
-	}
 	@Autowired
 	@Qualifier("dept1")  
-	public void setDept(HrDept dept)
-	{
-		System.out.println("Inside company setter dept method !! ");
-		this.dept = dept;
-		
-	}
+	HrDept dept;
 	
 	public void companyWork()
 	{
