73b3c050d48b5c1fcbc23501fb0e223255d82f2d
vikas <moganevikas@gmail.com>
2021-04-04 15:51:34 +0530

    autowiring using setter and constructor

--- AutoWiring/src/com/autowire/Company.java
+++ AutoWiring/src/com/autowire/Company.java
@@ -1,14 +1,23 @@
 package com.autowire;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Company {
 	HrDept dept;
+	
+	public Company() {}
+	
 	
 	public Company(HrDept dept)
 	{
 		this.dept = dept;
+		System.out.println(" company constructor !! ");
+
 		
 	}
-	
+	@Autowired
+	@Qualifier("dept1")  
 	public void setDept(HrDept dept)
 	{
 		System.out.println("Inside company setter dept method !! ");
