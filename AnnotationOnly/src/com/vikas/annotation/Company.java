34051900e566c5e28152b0dc16a53acbb8e2f0a3
vikas <moganevikas@gmail.com>
2021-04-04 16:28:36 +0530

    instead of bean we handle this using component annotation

---
+++ AnnotationOnly/src/com/vikas/annotation/Company.java
@@ -0,0 +1,12 @@
+package com.vikas.annotation;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Company {
+
+	public void showInfo()
+	{
+		System.out.println("hello details!");
+	}
+}
